318e51a6aa36062799d851cf64aae4439beaaa31
Soham Roy <roysoham58@gmail.com>
2021-04-11 21:45:48 +0530

    Add the backend portion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,53 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from 'axios';
+import qs from 'qs';
 
 function App() {
 
   const [notes, setNotes] = useState([]);
 
   function addNote(note) {
-    setNotes(prev => {
-      return [...prev, note];
-    });
+    const options = {
+      method: "POST",
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(note),
+      url: "http://localhost:3001"
+    };
+    axios(options)
+      .then(response => {
+        console.log(response);
+      });
   }
 
   function deleteNote(id) {
-    setNotes(prev => {
-      return prev.filter((note, index) => index !== id);
-    });
+    const options = {
+      method: "DELETE",
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify({'id': id}),
+      url: "http://localhost:3001"
+    };
+    axios(options)
+      .then(response => {
+        console.log(response);
+      });
   }
+
+  useEffect(() => {
+    axios.get("http://localhost:3001").then(response => {
+      // console.log(response);
+      setNotes(response.data);
+    });
+  }, [notes]);
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((note, index) => <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote} /> )}
+      {notes.map((note, index) => <Note key={note._id} id={note._id} title={note.title} content={note.content} onDelete={deleteNote} /> )}
       <Footer />
     </div>
   );
